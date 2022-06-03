55eac43347f76675ae407aaa9786a8e1ba61f9b7
Johan Ronnås <johan.ronnas@learnet.se>
2022-06-03 14:57:27 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function EnterCode(){
+    var inputValue = document.querySelector("#code-input").value;
+        
+    if(inputValue == "godis"){
+        document.querySelector("#output").style.display = "block";
+    }
+    
+}
